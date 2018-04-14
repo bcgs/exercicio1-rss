0bebeac51659710329f4adb2777605e544c60fbe
bcgs <bruno.bcgs@gmail.com>
2018-04-14 20:55:53 -0300

    Alteração da base para o exercício 2

--- RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
+++ RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
@@ -3,15 +3,18 @@ package br.ufpe.cin.if1001.rss.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
+import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.CursorAdapter;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.Toast;
 
 import org.xmlpull.v1.XmlPullParserException;
@@ -21,18 +24,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 import br.ufpe.cin.if1001.rss.R;
+import br.ufpe.cin.if1001.rss.db.SQLiteRSSHelper;
 import br.ufpe.cin.if1001.rss.util.adapter.XmlFeedAdapter;
 import br.ufpe.cin.if1001.rss.domain.ItemRSS;
 import br.ufpe.cin.if1001.rss.util.ParserRSS;
 
 public class MainActivity extends Activity {
-    // Deixou de ser constante pois agora pode ser modificado nas configurações
-    private String rss_feed;
-
     // Chave utilizada no SharedPreferences
     public static final String RSSFEED_KEY = "rssfeed";
 
@@ -48,6 +48,8 @@ public class MainActivity extends Activity {
     // Adapter customizado
     private XmlFeedAdapter adapter;
 
+    private SQLiteRSSHelper db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,16 +57,34 @@ public class MainActivity extends Activity {
 
         conteudoRSS = findViewById(R.id.items);
 
-        // Ao clicarmos em um ítem navegaremos pelo link disponibilizado pelo mesmo
-        conteudoRSS.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                ItemRSS item = (ItemRSS) adapter.getItem(position);
-                Intent intent = new Intent(
-                        Intent.ACTION_VIEW,
-                        Uri.parse(item.getLink())
+        SimpleCursorAdapter adapter =
+                new SimpleCursorAdapter(
+                        //contexto, como estamos acostumados
+                        this,
+                        //Layout XML de como se parecem os itens da lista
+                        R.layout.itemlista,
+                        //Objeto do tipo Cursor, com os dados retornados do banco.
+                        //Como ainda não fizemos nenhuma consulta, está nulo.
+                        null,
+                        //Mapeamento das colunas nos IDs do XML.
+                        // Os dois arrays a seguir devem ter o mesmo tamanho
+                        new String[]{SQLiteRSSHelper.ITEM_TITLE, SQLiteRSSHelper.ITEM_DATE},
+                        new int[]{R.id.item_titulo, R.id.item_data},
+                        //Flags para determinar comportamento do adapter, pode deixar 0.
+                        0
                 );
-                startActivity(intent);
+        //Seta o adapter. Como o Cursor é null, ainda não aparece nada na tela.
+        conteudoRSS.setAdapter(adapter);
+
+        // permite filtrar conteudo pelo teclado virtual
+        conteudoRSS.setTextFilterEnabled(true);
+
+        //Complete a implementação deste método de forma que ao clicar, o link seja aberto no navegador e
+        // a notícia seja marcada como lida no banco
+        conteudoRSS.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SimpleCursorAdapter adapter = (SimpleCursorAdapter) parent.getAdapter();
+                Cursor mCursor = ((Cursor) adapter.getItem(position));
             }
         });
     }
@@ -75,8 +95,14 @@ public class MainActivity extends Activity {
 
         // Sempre atualizando a ListView através do link disponibilizado
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        rss_feed = prefs.getString(RSSFEED_KEY, getString(R.string.rss_feed_default));
+        String rss_feed = prefs.getString(RSSFEED_KEY, getString(R.string.rss_feed_default));
         new CarregaRSStask().execute(rss_feed);
+    }
+
+    @Override
+    protected void onDestroy() {
+        db.close();
+        super.onDestroy();
     }
 
     // Menu de opções na barra de tarefas
@@ -93,46 +119,65 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * Display feed.
-     * @param feed Item list to be displayed.
-     */
-    private void displayFeed(List<ItemRSS> feed) {
-        adapter = new XmlFeedAdapter(this, feed);
-        conteudoRSS.setAdapter(adapter);
+    class CarregaRSStask extends AsyncTask<String, Void, Boolean> {
+
+        @Override
+        protected Boolean doInBackground(String... feeds) {
+            boolean flag_problema = false;
+            List<ItemRSS> items;
+            try {
+                String feed = getRssFeed(feeds[0]);
+                items = ParserRSS.parse(feed);
+                for (ItemRSS i : items) {
+                    Log.d("DB", "Buscando no Banco por link: " + i.getLink());
+                    ItemRSS item = db.getItemRSS(i.getLink());
+                    if (item == null) {
+                        Log.d("DB", "Encontrado pela primeira vez: " + i.getTitle());
+                        db.insertItem(i);
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                flag_problema = true;
+            } catch (XmlPullParserException e) {
+                e.printStackTrace();
+                flag_problema = true;
+            }
+            return flag_problema;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean teveProblema) {
+            if (teveProblema) {
+                Toast.makeText(MainActivity.this, "Houve algum problema ao carregar o feed.", Toast.LENGTH_SHORT).show();
+            } else {
+                //dispara o task que exibe a lista
+                new ExibirFeed().execute();
+            }
+        }
     }
 
-    private class CarregaRSStask extends AsyncTask<String, Void, List<ItemRSS>> {
+    class ExibirFeed extends AsyncTask<Void, Void, Cursor> {
+
         @Override
-        protected void onPreExecute() {
-            Toast.makeText(getApplicationContext(), "iniciando...", Toast.LENGTH_SHORT).show();
+        protected Cursor doInBackground(Void... voids) {
+            Cursor c = db.getItems();
+            c.getCount();
+            return c;
         }
 
         @Override
-        protected List<ItemRSS> doInBackground(String... params) {
-            List<ItemRSS> itemList = new ArrayList<>();
-            try {
-                itemList = ParserRSS.parse(getRssFeed(params[0]));
-            } catch (IOException | XmlPullParserException e) {
-                e.printStackTrace();
+        protected void onPostExecute(Cursor c) {
+            if (c != null) {
+                ((CursorAdapter) conteudoRSS.getAdapter()).changeCursor(c);
             }
-            return itemList;
-        }
-
-        // onPostExecute passa a receber uma List<ItemRSS>
-        @Override
-        protected void onPostExecute(List<ItemRSS> feed) {
-            Toast.makeText(getApplicationContext(), "terminando...", Toast.LENGTH_SHORT).show();
-
-            // Chamando função responsável por listar o feed
-            displayFeed(feed);
         }
     }
 
     //Opcional - pesquise outros meios de obter arquivos da internet
     private String getRssFeed(String feed) throws IOException {
         InputStream in = null;
-        String rssFeed = "";
+        String rssFeed;
         try {
             URL url = new URL(feed);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
